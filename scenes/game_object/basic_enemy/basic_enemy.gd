5a39c3d097e8c76df1400440cba3271a22c8436d
Victor Hugo Targino <vhtargino@gmail.com>
2025-03-06 18:32:21 -0300

    Implementação de arma e experiência

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -4,7 +4,7 @@ const MAX_SPEED = 40
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Area2D.area_entered.connect(on_area_entered)
+	%Area2D.area_entered.connect(on_area_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,14 +20,14 @@ func get_direction_to_player():
 		var enemy_position = (player_node.global_position - global_position).normalized()
 		
 		if global_position.x < player_node.global_position.x:
-			$Sprite2D.flip_h = true
+			%Sprite2D.flip_h = true
 		else:
-			$Sprite2D.flip_h = false
+			%Sprite2D.flip_h = false
 		
 		return enemy_position
 
 	return Vector2.ZERO
 
 
-func on_area_entered(other_area: Area2D):
+func on_area_entered(_other_area: Area2D):
 	queue_free()
